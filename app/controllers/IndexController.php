556decc46fc46115cdaf41f89af9312b4db54c47
Larisa Rozhniatovska <lara_rozn@urk.net>
2024-12-15 16:35:36 +0200

    add IndexController

---
+++ app/controllers/IndexController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace app\controllers;
+use app\core\AbstractController;
+
+class IndexController extends AbstractController
+{
+    /**
+     * displaying index.php
+     * @return void
+     */
+    public function index(): void
+    {
+        $this->view->render('index', [
+            'title' => 'Sign In',
+        ]);
+    }
+
+
+
+}

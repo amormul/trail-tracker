8da5edd002fa40f80f47ac718f7c0538ca2f213d
Nadin12 <pohlebina.odessa@gmail.com>
2024-12-17 23:44:31 +0200

    Create model

--- app/models/Inventory.php
+++ app/models/Inventory.php
@@ -2,5 +2,32 @@
 namespace app\models;
 class Inventory extends \app\core\AbstractDB
 {
-
+    //Create card with inventory
+    public function create(array $inventory): bool
+    {
+        $stmt = $this->db->prepare("INSERT INTO inventory (name, description, photo) VALUES (?, ?, ?)");
+        $stmt->bind_param("sss", $inventory['name'], $inventory['description'], $inventory['photo']);
+        return $stmt->execute();
+    }
+    //Update inventory card
+    public function update(array $inventory): bool
+    {
+        $stmt = $this->db->prepare("UPDATE inventory SET name = ?, description = ?, photo = ? WHERE id = ?");
+        $stmt->bind_param("sss", $inventory['name'], $inventory['description'], $inventory['photo']);
+        return $stmt->execute();
+    }
+    //Delete inventory card
+    public function delete(int $id): bool
+    {
+        $stmt = $this->db->prepare("DELETE FROM inventory WHERE id = ?");
+        $stmt->bind_param("i", $id);
+        return $stmt->execute();
+    }
+    //Get all inventory by id
+    public function getInventory(int $id): array
+    {
+        $stmt = $this->db->prepare("SELECT * FROM inventory WHERE id = ?");
+        $stmt->bind_param("i", $id);
+        $stmt->execute();
+    }
 }

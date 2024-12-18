e5aaf43e6161e7d3f145e609ba48ba450e06a4e6
Larisa Rozhniatovska <lara_rozn@urk.net>
2024-12-18 23:18:49 +0200

    edit RouteController.php

--- app/controllers/RouteController.php
+++ app/controllers/RouteController.php
@@ -18,17 +18,41 @@ class RouteController extends \app\core\AbstractController
     }
 
     /**
+     * reading the input data of the route
+     * @return array
+     */
+    public function inputData(): array
+    {
+        return [
+            'trip_id' => (int)filter_input(INPUT_POST, 'trip_id',FILTER_VALIDATE_INT) ?:
+                (int)$this->session->old['trip_id'],
+            'description' => filter_input(INPUT_POST, 'route_description') ?:
+                $this->session->old['route_description'],
+            'photo' => $_FILES['route_photo'],
+        ];
+    }
+
+    /**
+     * log entry about a specific error and show the page error.php
+     * @param string $message
+     * @return void
+     */
+    public function outputException(string $message) : void
+    {
+        //    запис в log про конкретну помилку
+        \app\core\Logs::write($message);
+        //    показати сторінку що щось пішло не так
+        $this->view->render('error', ['title' => 'oops', 'message' => $message]);
+    }
+
+    /**
      * creates route  in DB
      * @return void
      * @throws \Exception
      */
     public function store(): void
     {
-        $data = [
-            'trip_id' => (int)filter_input(INPUT_POST, 'trip_id'),
-            'description' => filter_input(INPUT_POST, 'description'),
-            'photo' => filter_input(INPUT_POST, 'photo'),
-        ];
+        $data = $this->inputData();
         $errors = \app\core\RouteValidators::validateRoute($data);
         $this->session->trip_id = $data['trip_id'];
         if (!empty($errors)) {
@@ -38,9 +62,7 @@ class RouteController extends \app\core\AbstractController
             try {
                 $res = $this->route->create($data);
             } catch (Exception $e) {
-                \app\core\Logs::write($e->getMessage());
-                //    показати сторінку що щось пішло не так
-                $this->view->render('error', ['title' => 'oops', 'message' => $e->getMessage()]);
+                $this->outputException($e->getMessage());
             }
             \app\core\Route::redirect('/index/show');
         }
@@ -53,11 +75,7 @@ class RouteController extends \app\core\AbstractController
      */
     public function update(): void
     {
-        $data = [
-            'trip_id' => (int)filter_input(INPUT_POST, 'trip_id'),
-            'description' => filter_input(INPUT_POST, 'description'),
-            'photo' => filter_input(INPUT_POST, 'photo'),
-        ];
+        $data = $this->inputData();
         $errors = \app\core\RouteValidators::validateRoute($data);
         $this->session->trip_id = $data['trip_id'];
         if (!empty($errors)) {
@@ -67,10 +85,7 @@ class RouteController extends \app\core\AbstractController
             try {
                 $res = $this->route->update($data);
             } catch (Exception $e) {
-                //    запис в log про конкретну помилку
-                \app\core\Logs::write($e->getMessage());
-                //    показати сторінку що щось пішло не так
-                $this->view->render('error', ['title' => 'oops', 'message' => $e->getMessage()]);
+                $this->outputException($e->getMessage());
             }
             \app\core\Route::redirect('/index/show');
         }
@@ -82,17 +97,15 @@ class RouteController extends \app\core\AbstractController
      */
     public function like(): void
     {
-        $trip_id = (int)filter_input(INPUT_POST, 'trip_id');
+        $trip_id = (int)filter_input(INPUT_POST, 'trip_id')?:
+            (int)$this->session->old['trip_id'];
         $user_id = $this->getCurrentUserId();
         $route = $this->route->getByTripId($trip_id);
         $this->session->trip_id = $trip_id;
         try {
             $res = $this->route->like($route['id'], $user_id);
         } catch (\Exception $e){
-            //    запис в log про конкретну помилку
-            \app\core\Logs::write($e->getMessage());
-            //    показати сторінку що щось пішло не так
-            $this->view->render('error', ['title' => 'oops', 'message' => $e->getMessage()]);
+            $this->outputException($e->getMessage());
         }
         \app\core\Route::redirect('/index/show');
     }

81d657928115a3dda4e437ee46d518f1149e904b
Nadin12 <pohlebina.odessa@gmail.com>
2024-12-19 18:10:46 +0200

    add php

--- app/views/pages/inventory.php
+++ app/views/pages/inventory.php
@@ -10,36 +10,10 @@
             <div class="row">
                 <div class="col-md-4 mb-4">
                     <div class="card h-100">
-                        <img src="https://via.placeholder.com/300x200" class="card-img-top" alt="Backpack">
+                        <img src="<?= $inventory['photo'] ?>" alt="<?= $inventory['name'] ?>" class="card-img-top">
                         <div class="card-body d-flex flex-column">
-                            <h5 class="card-title">Tent</h5>
-                            <p class="card-text">A lightweight 2-person tent, perfect for hiking trips.</p>
-                            <div class="mt-auto d-flex justify-content-end">
-                                <button class="btn btn-primary btn-sm mr-2">Update</button>
-                                <button class="btn btn-danger btn-sm">Delete</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4 mb-4">
-                    <div class="card h-100">
-                        <img src="https://via.placeholder.com/300x200" class="card-img-top" alt="Backpack">
-                    <div class="card-body d-flex flex-column">
-                        <h5 class="card-title">Sleeping Bag</h5>
-                    <p class="card-text">Warm and comfortable sleeping bag for all seasons.</p>
-                        <div class="mt-auto d-flex justify-content-end">
-                            <button class="btn btn-primary btn-sm mr-2">Update</button>
-                            <button class="btn btn-danger btn-sm">Delete</button>
-                        </div>
-                    </div>
-                    </div>
-                </div>
-                <div class="col-md-4 mb-4">
-                    <div class="card h-100">
-                    <img src="https://via.placeholder.com/300x200" class="card-img-top" alt="Backpack">
-                        <div class="card-body d-flex flex-column">
-                                <h5 class="card-title">Backpack</h5>
-                                <p class="card-text">Durable 40L backpack with multiple compartments.</p>
+                            <h5 class="card-title"><?= $inventory['name'] ?></h5>
+                            <p class="card-text"><?= $inventory['descriptions'] ?></p>
                             <div class="mt-auto d-flex justify-content-end">
                                 <button class="btn btn-primary btn-sm mr-2">Update</button>
                                 <button class="btn btn-danger btn-sm">Delete</button>

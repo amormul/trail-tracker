d58d2284d8d105330e8c7f4b75884aeffc8e084f
Kagrtavy <nesterivan97@gmail.com>
2024-12-20 14:43:24 +0200

    new edit

--- app/views/pages/edit.php
+++ app/views/pages/edit.php
@@ -1,17 +1,28 @@
 <div class="container mt-5 pt-4 mb-5">
+    <!-- Back Button -->
     <a href="<?= \app\core\Route::url("gallery", "photo", [$photo['id']]) ?>" class="btn btn-primary text-white btn-sm mr-2">Back</a>
-    <form action="<?= \app\core\Route::url("gallery", "savePhoto", [$photo['id']]) ?>" method="POST" enctype="multipart/form-data">
-        <!-- For photo -->
-        <div class="form-group mt-3">
-            <label for="photoFile">Change Photo</label>
-            <input type="file" id="photoFile" name="file" class="form-control" accept="image/*">
+    <!-- Form -->
+    <form action="<?= \app\core\Route::url("gallery", "savePhoto", [$photo['id']]) ?>" method="POST" enctype="multipart/form-data" class="row mt-4 bg-light shadow rounded p-4">
+        <!-- Left Part: Photo -->
+        <div class="col-md-6 text-center">
+            <div class="form-group">
+                <img src="<?= $photo['photo'] ?>" alt="Current Photo" class="img-fluid rounded shadow mb-3">
+            </div>
+            <div class="form-group">
+                <label for="photoFile" class="form-label">New Photo:</label>
+                <input type="file" id="photoFile" name="file" class="form-control" accept="image/*">
+            </div>
         </div>
-        <!-- For description -->
-        <div class="form-group mb-3">
-            <label for="photoDescription">Description</label>
-            <textarea id="photoDescription" name="description" class="form-control" rows="4"><?= $photo['comment'] ?></textarea>
+        <!-- Right Part: Description -->
+        <div class="col-md-6">
+            <div class="form-group">
+                <label for="photoDescription" class="form-label">Description</label>
+                <textarea id="photoDescription" name="description" class="form-control" rows="8"><?= $photo['comment'] ?></textarea>
+            </div>
         </div>
-        <button type="submit" class="btn btn-success text-white btn-sm mr-2">Save</button>
+        <!-- Save Button -->
+        <div class="col-12 text-right mt-3">
+            <button type="submit" class="btn btn-success text-white btn-sm">Save</button>
+        </div>
     </form>
 </div>
-

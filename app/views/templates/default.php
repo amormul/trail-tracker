0d2fd46c13f4958e7f62f7f8e26ca9c4e42e06fa
Kagrtavy <nesterivan97@gmail.com>
2024-12-20 12:12:20 +0200

    changes in default.php

--- app/views/templates/default.php
+++ app/views/templates/default.php
@@ -6,14 +6,17 @@
               content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title><?= $title?></title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-              integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet"/>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/css/select2.min.css" rel="stylesheet" />
         <script src="https://kit.fontawesome.com/d16cee9f8d.js"></script>
+        <link href="<?= DIRECTORY_SEPARATOR . 'css' . DIRECTORY_SEPARATOR . 'style.css' ?>" rel="stylesheet" />
     </head>
     <body>
         <?php include_once self::VIEWS_DIR . 'common' . DIRECTORY_SEPARATOR . 'header.php'?>
+<!--        --><?php //include_once self::VIEWS_DIR . 'common' . DIRECTORY_SEPARATOR . 'ad.php' ?>
         <?php include_once self::VIEWS_DIR . 'pages' . DIRECTORY_SEPARATOR . $page . '.php'?>
         <?php include_once self::VIEWS_DIR . 'common' . DIRECTORY_SEPARATOR . 'footer.php'?>
+        <script src="<?= DIRECTORY_SEPARATOR . 'js' . DIRECTORY_SEPARATOR . 'scripts.js' ?>"></script>
     </body>
-<!--    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>-->
+
 </html>

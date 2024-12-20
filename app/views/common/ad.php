53acb8b4624cfbfc4935597ef419e2254af1c1fa
Kagrtavy <nesterivan97@gmail.com>
2024-12-20 14:45:15 +0200

    init ad

---
+++ app/views/common/ad.php
@@ -0,0 +1,5 @@
+<div class="position-fixed ad-section d-none d-md-block">
+    <div class="d-flex h-100 align-items-center justify-content-center">
+        <span class="text-secondary font-weight-bold">AD</span>
+    </div>
+</div>

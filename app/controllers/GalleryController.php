74781676ef498c7d1dd1654bfb5811fd751bbd5e
Kagrtavy <nesterivan97@gmail.com>
2024-12-22 11:42:21 +0200

    new method

--- app/controllers/GalleryController.php
+++ app/controllers/GalleryController.php
@@ -1,6 +1,25 @@
 <?php
 
-class GalleryController extends \app\core\AbstractController
-{
+namespace app\controllers;
 
+use app\core\AbstractController;
+use app\core\Helpers;
+use app\core\Route;
+use app\core\Session;
+use app\models\Gallery;
+
+class GalleryController extends AbstractController
+{
+    /**
+     * opens add_photo page
+     * @return void
+     */
+    public function addPhoto(): void
+    {
+        $tripId = filter_input(INPUT_GET, 'trip_id', FILTER_VALIDATE_INT);
+        $this->view->render('add_photo', [
+            'title' => 'Add New Photo',
+            'tripId' => $tripId
+        ]);
+    }
 }

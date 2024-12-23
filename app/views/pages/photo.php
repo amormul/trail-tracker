77b2cddd00bc76e68206184937560c08d8c094e2
Kagrtavy <nesterivan97@gmail.com>
2024-12-23 15:23:05 +0200

    changes

--- app/views/pages/photo.php
+++ app/views/pages/photo.php
@@ -10,18 +10,21 @@
     <div class="row bg-light shadow rounded py-4 px-4">
         <!-- Left part -->
         <div class="col-md-4 text-center">
-            <img src="<?= $photo['photo'] ?>" alt="Photo" class="img-fluid rounded shadow">
+            <img src="<?= $photo['photo'] ?>" alt="Photo" class="img-fluid rounded shadow" style="width: 400px; height: auto; display: block; margin: 0 auto;">
         </div>
         <!-- Right part -->
         <div class="col-md-8 d-flex flex-column justify-content-start">
             <!-- Author -->
             <h3 class="text-primary"><?= $photo['author'] ?></h3>
             <!-- Description -->
-            <p class="font-weight-bold">Description:</p>
+            <p class="font-weight-bold">Comment:</p>
             <p><?= $photo['comment'] ?></p>
-            <div class="mt-auto">
-                <button class="btn btn-outline-danger btn-sm">❤</button>
-                <span>10 Likes</span>
+            <div class="d-flex align-items-center mt-2">
+                <form action="<?= app\core\Route::url('gallery', 'like') ?>" method="post" class="d-flex align-items-center">
+                    <input type="hidden" name="photo_id" value="<?= $photo['id'] ?>">
+                    <input type="submit" class="btn btn-outline-danger btn-sm me-2" value="&#x2764;">
+                </form>
+                <span>Likes: <?= $photo['likes'] ?></span>
             </div>
         </div>
     </div>

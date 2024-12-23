498fbfad18004c000a8861318e12199fe31cabfb
amormul <ghafics@gmail.com>
2024-12-23 14:00:45 +0200

    add user login/registration

--- app/core/Session.php
+++ app/core/Session.php
@@ -4,6 +4,8 @@ namespace app\core;
 
 class Session
 {
+    private array $properties = [];
+
     public function __construct()
     {
         $this->start();
@@ -14,16 +16,32 @@ class Session
         session_start();
     }
 
+    /**
+     * Write property to session
+     * @param string $key
+     * @param mixed $data
+     * @return void
+     */
     public function __set(string $key, $data): void
     {
         $_SESSION[$key] = $data;
     }
 
+    /**
+     * Get property from session
+     * @param string $key
+     * @return mixed
+     */
     public function __get(string $key)
     {
         return $_SESSION[$key] ?? null;
     }
 
+    /**
+     * Remove property from session
+     * @param string $key
+     * @return void
+     */
     public function remote(string $key): void
     {
         if (!empty($_SESSION[$key])) {
@@ -31,6 +49,10 @@ class Session
         }
     }
 
+    /**
+     * Destroy session
+     * @return void
+     */
     public function destroy(): void
     {
         session_destroy();

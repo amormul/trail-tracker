b332608deda82405212ea9c544b80ed25b2ab9e6
Kagrtavy <nesterivan97@gmail.com>
2024-12-23 15:22:34 +0200

    added like methods

--- app/models/Gallery.php
+++ app/models/Gallery.php
@@ -22,6 +22,7 @@ class Gallery extends AbstractDB
     }
 
     /**
+     * get photo info
      * @param int $photoId
      * @return array|null
      */
@@ -37,6 +38,13 @@ class Gallery extends AbstractDB
         return $result->fetch_assoc();
     }
 
+    /**
+     * change photo info
+     * @param int $photoId
+     * @param string $photoPath
+     * @param string $comment
+     * @return bool
+     */
     public function edit(int $photoId, string $photoPath, string $comment): bool
     {
         $query = "UPDATE gallery SET photo = ?, comment = ? 
@@ -60,5 +68,64 @@ class Gallery extends AbstractDB
         $stmt = $this->db->prepare($query);
         $stmt->bind_param("isis", $userId, $photo, $tripId, $comment);
         return $stmt->execute();
+    }
+
+    /**
+     * checks if a user has liked a specific photo
+     * @param int $photoId
+     * @param int $userId
+     * @return bool
+     */
+    public function checkLike(int $photoId, int $userId): bool
+    {
+        $stmt = $this->db->prepare("SELECT COUNT(*) as count FROM likes_photo WHERE photo_id = ? AND user_id = ?");
+        $stmt->bind_param("ii", $photoId, $userId);
+        $stmt->execute();
+        $result = $stmt->get_result()->fetch_assoc();
+
+        if ($result['count'] > 0) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * adds a like to a trip from a specific user
+     * @param int $photoId
+     * @param int $userId
+     * @return bool
+     */
+    public function addLike(int $photoId, int $userId): bool
+    {
+        $stmt = $this->db->prepare("INSERT INTO likes_photo (photo_id, user_id) VALUES (?, ?)");
+        $stmt->bind_param("ii", $photoId, $userId);
+        return $stmt->execute();
+    }
+
+    /**
+     * removes a like from a trip for a specific user
+     * @param int $photoId
+     * @param int $userId
+     * @return bool
+     */
+    public function deleteLike(int $photoId, int $userId): bool
+    {
+        $stmt = $this->db->prepare("DELETE FROM likes_photo WHERE photo_id = ? AND user_id = ?");
+        $stmt->bind_param("ii", $photoId, $userId);
+        return $stmt->execute();
+    }
+
+    /**
+     * counts the number of likes for a specific photo
+     * @param int $photoId
+     * @return int
+     */
+    public function countLikes(int $photoId): int
+    {
+        $stmt = $this->db->prepare("SELECT COUNT(*) as count FROM likes_photo WHERE photo_id = ?");
+        $stmt->bind_param("i", $photoId);
+        $stmt->execute();
+        $result = $stmt->get_result()->fetch_assoc();
+        return $result['count'] ?? 0;
     }
 }
